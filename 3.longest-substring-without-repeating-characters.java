e4a6f0b61ac9279a2a0f58262a6cb0a9e50a7f87
zyy0098 <770823669@qq.com>
2023-01-02 14:43:45 -0500

    3-simple sliding window

--- 3.longest-substring-without-repeating-characters.java
+++ 3.longest-substring-without-repeating-characters.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+
 /*
  * @lc app=leetcode id=3 lang=java
  *
@@ -7,7 +10,37 @@
 // @lc code=start
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        HashMap<Character,Integer> window = new HashMap<Character,Integer>();
+        // 2 start point. left start at 0, right start at 0 
+        int left = 0, right = 0; 
+        int res = 0; // 结果
+
+        // right point extend while not going at the end
+        while(right < s.length()){
+            // get the character of s in index right
+            char c = s.charAt(right);
+            right ++;
+            
+            // if c not in window then ++, else add c into
+            if(window.get(c) != null){
+                window.put(c, window.get(c) + 1);
+            }else{
+                window.put(c, 1);
+            }
+            
+            // if any count of char > 1, stop right from extending
+            // and make every count of char minus 1.
+            // Until left coinside with right, and count of this char is 1.
+            while( window.get(c) > 1){
+                char d = s.charAt(left);
+                left ++;
+                window.put(d, window.get(d) - 1);
+            }
+
+            // get the longest substring
+            res = Math.max(res, right - left);
+        }
+        return res;
     }
 }
 // @lc code=end
